6311b6b7c120c733e5877722c1d5af3a2894c221
khushi pal <165002584+khushipal-01@users.noreply.github.com>
2026-02-07 16:26:00 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,7 @@
+const BASE_URL = "https://suspicious-link-detector-y50y.onrender.com";
+
 export async function checkURL(url) {
-  const response = await fetch("http://127.0.0.1:8000/check-url", {
+  const response = await fetch(`${BASE_URL}/check-url`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
